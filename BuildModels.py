aeff07ceccd416bfea5333bf1bb9f5a3337f258e
suchibrata <mailto.dutta.joy@gmail.com>
2019-10-09 21:34:35 +0530

    Adding code for multistep prediction

---
+++ BuildModels.py
@@ -0,0 +1,45 @@
+import LSTM as lstm
+import pandas as pd
+import indicators as i
+
+n_timesteps=60
+dataset=pd.read_csv('./historical_daily_data/tata_historical.csv')
+
+close_dataset,n_features=lstm.prepare_data(input_df=dataset,y='Close',run_function=i.add_default_indicators,
+                                drop_columns=['Date','Open','High','Low','Adj Close','Volume'],
+                                start_row=60)
+scaled_data=lstm.create_scaled_data(input_df=close_dataset,model_file='tm_close_minmax.pkl')
+reframed=lstm.series_to_supervised(data=scaled_data,n_in=n_timesteps)
+lstm_regressor=lstm.build_lstm_model(input_shape=(n_timesteps,n_features))
+lstm_regressor=lstm.fit_lstm_model(model_file='tm_close_lstm.h5',regressor=lstm_regressor,supervised_df=reframed,
+                                   n_features=n_features,n_timesteps=n_timesteps,epochs=1)
+
+
+open_dataset,n_features=lstm.prepare_data(input_df=dataset,y='Open',run_function=i.add_default_indicators,
+                                drop_columns=['Date','Close','High','Low','Adj Close','Volume'],
+                                start_row=60)
+scaled_data=lstm.create_scaled_data(input_df=close_dataset,model_file='tm_open_minmax.pkl')
+reframed=lstm.series_to_supervised(data=scaled_data,n_in=n_timesteps)
+lstm_regressor=lstm.build_lstm_model(input_shape=(n_timesteps,n_features))
+lstm_regressor=lstm.fit_lstm_model(model_file='tm_open_lstm.h5',regressor=lstm_regressor,supervised_df=reframed,
+                                   n_features=n_features,n_timesteps=n_timesteps,epochs=1)
+
+
+high_dataset,n_features=lstm.prepare_data(input_df=dataset,y='High',run_function=i.add_default_indicators,
+                                drop_columns=['Date','Close','Open','Low','Adj Close','Volume'],
+                                start_row=60)
+scaled_data=lstm.create_scaled_data(input_df=close_dataset,model_file='tm_high_minmax.pkl')
+reframed=lstm.series_to_supervised(data=scaled_data,n_in=n_timesteps)
+lstm_regressor=lstm.build_lstm_model(input_shape=(n_timesteps,n_features))
+lstm_regressor=lstm.fit_lstm_model(model_file='tm_high_lstm.h5',regressor=lstm_regressor,supervised_df=reframed,
+                                   n_features=n_features,n_timesteps=n_timesteps,epochs=1)
+
+low_dataset,n_features=lstm.prepare_data(input_df=dataset,y='Low',run_function=i.add_default_indicators,
+                                drop_columns=['Date','Close','Open','High','Adj Close','Volume'],
+                                start_row=60)
+scaled_data=lstm.create_scaled_data(input_df=close_dataset,model_file='tm_low_minmax.pkl')
+reframed=lstm.series_to_supervised(data=scaled_data,n_in=n_timesteps)
+lstm_regressor=lstm.build_lstm_model(input_shape=(n_timesteps,n_features))
+lstm_regressor=lstm.fit_lstm_model(model_file='tm_low_lstm.h5',regressor=lstm_regressor,supervised_df=reframed,
+                                   n_features=n_features,n_timesteps=n_timesteps,epochs=1)
+
---
+++ MultiStepPredict.py
@@ -0,0 +1,98 @@
+import LSTM as lstm
+import pandas as pd
+from copy import deepcopy
+import indicators as i
+
+next_index=0
+n_timesteps=60
+n_steps=15
+
+def get_next_data(recent_dataset):
+    global next_index
+    print(next_index)
+    print(recent_dataset.shape[0])
+    if recent_dataset.shape[0] > next_index:
+        row=recent_dataset.iloc[next_index]
+        row=row.to_dict()
+        print(row)
+        #row=row[0]
+        next_index+=1
+    else:
+        row=None
+    return row
+
+if __name__=='__main__':
+    close_scaler=lstm.load_scaler('tm_close_minmax.pkl')
+    close_lstm=lstm.load_lstm('tm_close_lstm.h5')
+
+    open_scaler=lstm.load_scaler('tm_open_minmax.pkl')
+    open_lstm=lstm.load_lstm('tm_open_lstm.h5')
+
+    high_scaler=lstm.load_scaler('tm_high_minmax.pkl')
+    high_lstm=lstm.load_lstm('tm_high_lstm.h5')
+
+    low_scaler=lstm.load_scaler('tm_low_minmax.pkl')
+    low_lstm=lstm.load_lstm('tm_low_lstm.h5')
+
+    historical_dataset=pd.read_csv('./historical_daily_data/tata_historical.csv')
+    historical_dataset=historical_dataset.tail(365)
+    recent_dataset=pd.read_csv('./historical_daily_data/tata_recent.csv')
+    next_index=recent_dataset.shape[0]-2
+
+
+
+    c_df=pd.DataFrame()
+    while True:
+        row=get_next_data(recent_dataset)
+        print(next_index)
+        print(row)
+
+        if not row:
+            break
+        historical_dataset=historical_dataset.append(row,ignore_index=True)
+        working_dataset=deepcopy(historical_dataset)
+        close_prices=[]
+
+        for idx in range(n_steps):
+            close_dataset, n_features = lstm.prepare_data(input_df=working_dataset, y='Close', run_function=i.add_default_indicators,
+                                                          drop_columns=['Date', 'Open', 'High', 'Low', 'Adj Close', 'Volume'],
+                                                          start_row=60)
+            predicted_close=lstm.predict(close_lstm,close_dataset,n_features,n_timesteps,close_scaler)
+
+            open_dataset, n_features = lstm.prepare_data(input_df=working_dataset, y='Open', run_function=i.add_default_indicators,
+                                                         drop_columns=['Date', 'Close', 'High', 'Low', 'Adj Close', 'Volume'],
+                                                         start_row=60)
+            predicted_open = lstm.predict(open_lstm, open_dataset, n_features, n_timesteps, open_scaler)
+
+            high_dataset, n_features = lstm.prepare_data(input_df=working_dataset, y='High', run_function=i.add_default_indicators,
+                                                         drop_columns=['Date', 'Close', 'Open', 'Low', 'Adj Close', 'Volume'],
+                                                         start_row=60)
+            predicted_high = lstm.predict(high_lstm, high_dataset, n_features, n_timesteps, high_scaler)
+
+            low_dataset, n_features = lstm.prepare_data(input_df=working_dataset, y='Low',
+                                                         run_function=i.add_default_indicators,
+                                                         drop_columns=['Date', 'Close', 'Open', 'High', 'Adj Close', 'Volume'],
+                                                         start_row=60)
+            predicted_low = lstm.predict(low_lstm, low_dataset, n_features, n_timesteps, low_scaler)
+
+            o,h,l,c=predicted_open[-1],predicted_high[-1],predicted_low[-1],predicted_close[-1]
+
+            h=max(o,h,l,c)
+            l=min(o,h,l,c)
+            v=0
+            adj_c=0
+            pd='T{}'.format(idx+1)
+            row.update({pd:c})
+            close_prices.append(c)
+            working_dataset=working_dataset.append({'Open':o,'Close':c,'High':h,'Low':l,'Adj Close':adj_c,'Volume':v},ignore_index=True)
+        print(row)
+        c_df=c_df.append(row,ignore_index=True)
+    c_df.to_csv('tata_motors_prediction.csv')
+
+
+
+
+
+
+
+
