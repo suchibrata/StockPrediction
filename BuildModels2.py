87d124d8bf32e90394903da61ab1f86d0b689eee
Ubuntu <ubuntu@ip-172-31-27-244.ap-southeast-1.compute.internal>
2019-10-22 15:18:06 +0000

    Failure

--- BuildModels2.py
+++ BuildModels2.py
@@ -3,7 +3,7 @@ import pandas as pd
 import indicators as i
 
 n_timesteps=180
-dataset=pd.read_csv('./historical_daily_data/tata_historical.csv')
+dataset=pd.read_csv('./historical_daily_data/sbin_historical.csv')
 n_steps=15
 start_row=180
 epochs=50
@@ -13,7 +13,7 @@ for idx in range(n_steps):
                                     drop_columns=['Date','Open','High','Low','Adj Close','Volume'],
                                     start_row=start_row)
     scaled_data=lstm.create_scaled_data(input_df=close_dataset,model_file='sbin_close_minmax_t{}.pkl'.format(idx+1))
-    reframed=lstm.series_to_supervised_t(data=scaled_data,n_in=n_timesteps,n_out=idx+1)
+    reframed=lstm.series_to_supervised(data=scaled_data,n_in=n_timesteps,n_out=idx+1)
     lstm_regressor=lstm.build_lstm_model(input_shape=(n_timesteps,n_features))
     lstm_regressor=lstm.fit_lstm_model(model_file='sbin_close_lstm_t{}.h5'.format(idx+1),regressor=lstm_regressor,
                                        supervised_df=reframed,n_features=n_features,n_timesteps=n_timesteps,epochs=epochs)
